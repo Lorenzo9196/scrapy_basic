c99031b6658fec79f951055cc779aeb897245e36
Lore <lorenzo9196@hotmail.com>
2022-03-12 21:41:23 -0500

    mejorando la spider ahora imprime nota, autor y tags

--- spiders/quotes.py
+++ spiders/quotes.py
@@ -7,8 +7,21 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        h1_tag = response.xpath("//h1/a/text()").extract()
-        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        # h1_tag = response.xpath("//h1/a/text()").extract()
+        # tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
 
-        yield {'H1_tag': h1_tag, 'Tags':tags}
+        # yield {'H1_tag': h1_tag, 'Tags':tags}
+
+        quotes = response.xpath('//*[@class="quote"]')
+
+        for quote in quotes:
+            text = quote.xpath('./*[@class="text"]/text()').extract() 
+            author = quote.xpath('.//*[@class="author"]/text()').extract()         
+            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract()
+
+            print("/n")
+            print(text)
+            print(author)
+            print(tags)
+            print("/n")
 
